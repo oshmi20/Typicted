d75cff4b88550aef657b115008d42a88691ce4de
Oshmi Thakur <88018239+oshmi20@users.noreply.github.com>
2024-02-20 18:39:44 +0530

    Update Game1.js

--- Game1.js
+++ Game1.js
@@ -86,34 +86,15 @@ function initTyping() {
       timer = setInterval(initTimer, 1000);
       isTyping = true;
     }
-    document.addEventListener("keydown", KeyCheck);
-    function KeyCheck(event) {
-      KeyID = event.keyCode;
-      switch (KeyID) {
-        case 8:
-          inpField.disabled = false;
-          if (mistakes > 0) {
-            if (characters[charIndex].classList.contains("incorrect")) {
-              mistakes--;
-            }
-            characters[charIndex].classList.remove("correct", "incorrect");
-          }
-          characters[charIndex + 1].style.color = "#868686";
-          characters[charIndex + 1].style.fontSize = "2" + "rem";
-          characters[charIndex + 1].style.fontWeight = 500;
-          characters[charIndex + 1].style.borderBottom = "thick solid transparent";
-
-          characters[charIndex].style.color = "#17a2b8";
-          characters[charIndex].style.fontSize = "2" + "rem";
-          characters[charIndex].style.fontWeight = 900;
-          characters[charIndex].style.borderBottom = "thick solid #17a2b8";
-
-          break;
-        default:
-          break;
-      }
-    }
+    
     if (typedChar == null) {
+        if(charIndex > 0) {
+      charIndex--;
+      if(characters[charIndex].classList.contains("incorrect")) {
+          mistakes--;
+      }
+      characters[charIndex].classList.remove("correct", "incorrect");
+  }
     }
     else {
       if (characters[charIndex].innerText === typedChar) {
@@ -215,25 +196,15 @@ function initTyping() {
       else {
         wrongword.play();
         mistakes++;
-        characters[charIndex].classList.add("incorrect");
-        characters[charIndex].style.color = "red";
-        characters[charIndex].style.fontSize = "1.5" + "rem";
-        characters[charIndex].style.fontWeight = 0;
-        characters[charIndex].style.borderBottom = "thick solid transparent";
-        characters[charIndex + 1].style.color = "#17a2b8";
-        characters[charIndex + 1].style.fontSize = "2" + "rem";
-        characters[charIndex + 1].style.fontWeight = 900;
-        characters[charIndex + 1].style.borderBottom = "thick solid #17a2b8";
-
+  characters[charIndex].classList.add("incorrect");
+        inpField.value = inpField.value.slice(0, -1);
+        charIndex--;
+        mistakes--;
         wheel1.classList.remove("Running")
         wheel2.classList.remove("Running")
-        inpField.value = inpField.value.slice(0, -1);
-        inpField.disabled = true;
 
       }
-      if (mistakes == 0) {
         charIndex++;
-      }
 
 
     }
